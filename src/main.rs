4a4c26f312f81535fa4fd1fa6a35a788ac2426ed
rn7s2 <mail@rn7s2.cn>
2022-11-29 03:53:49 +0800

    add output dir.

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,31 @@
 use clap::Parser;
 use std::{
     io::{Read, Write},
-    path::{Path, PathBuf},
+    path::PathBuf,
 };
 
 #[derive(Parser, Debug)]
 struct Args {
+    #[arg(long, short)]
+    outdir: Option<String>,
     files: Vec<String>,
 }
 
-fn reverse_filename(path: &str) -> String {
-    let path = Path::new(path);
-    let mut result: PathBuf = path.to_path_buf();
-    result.set_file_name(
-        path.file_name()
-            .unwrap()
-            .to_str()
-            .unwrap()
-            .chars()
-            .rev()
-            .collect::<String>(),
-    );
-    result.to_str().unwrap().to_string()
+fn reverse_filename(path: &PathBuf) -> String {
+    path.file_name()
+        .unwrap()
+        .to_str()
+        .unwrap()
+        .chars()
+        .rev()
+        .collect::<String>()
 }
 
-fn reverse_file(path: &str) {
+fn reverse_file(path: &PathBuf, outdir: &PathBuf) {
     let mut file = std::fs::File::open(path).unwrap();
-    let mut reverse_file = std::fs::File::create(reverse_filename(path)).unwrap();
+    let mut outdir = outdir.clone();
+    outdir.push(reverse_filename(path));
+    let mut reverse_file = std::fs::File::create(outdir).unwrap();
     let mut buf = Vec::new();
     file.read_to_end(&mut buf).unwrap();
     buf.reverse();
@@ -35,8 +34,20 @@ fn reverse_file(path: &str) {
 
 fn main() {
     let args = Args::parse();
+    let outdir = match args.outdir {
+        Some(dir) => {
+            let dir = PathBuf::from(dir);
+            assert!(dir.is_dir());
+            assert!(dir.exists());
+            dir
+        }
+        None => PathBuf::from("."),
+    };
 
     for file in &args.files {
-        reverse_file(file);
+        let path = PathBuf::from(file);
+        assert!(path.is_file());
+        assert!(path.exists());
+        reverse_file(&path, &outdir);
     }
 }

e6496743b3f73f6447345c67cb35c6670ec34996
rn7s2 <mail@rn7s2.cn>
2022-11-29 03:34:02 +0800

    add reverse file name.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,32 @@
 use clap::Parser;
-use std::io::{Read, Write};
+use std::{
+    io::{Read, Write},
+    path::{Path, PathBuf},
+};
 
 #[derive(Parser, Debug)]
 struct Args {
     files: Vec<String>,
 }
 
+fn reverse_filename(path: &str) -> String {
+    let path = Path::new(path);
+    let mut result: PathBuf = path.to_path_buf();
+    result.set_file_name(
+        path.file_name()
+            .unwrap()
+            .to_str()
+            .unwrap()
+            .chars()
+            .rev()
+            .collect::<String>(),
+    );
+    result.to_str().unwrap().to_string()
+}
+
 fn reverse_file(path: &str) {
     let mut file = std::fs::File::open(path).unwrap();
-    let mut reverse_file = std::fs::File::create(path.to_owned() + ".rev").unwrap();
+    let mut reverse_file = std::fs::File::create(reverse_filename(path)).unwrap();
     let mut buf = Vec::new();
     file.read_to_end(&mut buf).unwrap();
     buf.reverse();
